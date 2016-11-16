4a9842518e96b79c9b193e0040adcc29e65f6f53
xiaeryu <xiaeryu@nus.edu.sg>
2016-11-16 21:46:08 +0800

    Update plot-pval.r

--- plot-pval.r
+++ plot-pval.r
@@ -2,7 +2,7 @@
 data <- read.table("final_output")
 
 # Save to a png file
-png("Summary.plot.rearrange.png", height=960, width=2000)
+png("Summary.plot.png", height=960, width=2000)
 
 # Plot figure
 plot(data[,1]~seq(1,nrow(data)),ylab="-log10P",las=1,pch=20)
